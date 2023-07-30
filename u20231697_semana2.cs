e2fc0d14b75bab2e217c72c4ed3a63f6dce171da
Navidad <pato2002slv@gmail.com>
2023-07-30 02:51:06 -0500

    guia numero 2

---
+++ u20231697_semana2.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Declaracion de 4 variables tipo string
+
+            string nombre = "pedro";
+            string nombre = "Antonio";
+            string apellido = "Alvarez";
+            string apellido = "Hernandez";
+
+            // Declaracion de 3 variables tipo integer
+            
+            int edad = 21;
+            int edad = 35;
+            int edad = 48;
+
+            // Declaracion de 3 variables tipo float
+
+            float prestamo = 650.60;
+            float compra = 751.98;
+            float deuda = 225.65;
+
+            // Declaracion de 2 variables tipo boolean
+
+            bool estudiante = true;
+            bool becado = false;
+
+            //Autoevaluacion 10
+        }
+        
+
+
+
+
+    }
+}
